aade4bcf0b3f492e2a6d2234fabc34fec67c2803
Milt Deherrera <miltdeherrera@gmail.com>
2022-12-07 14:22:14 -0800

    Refactor code to destructure function arguments

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
-const request = require('request')
 const forecast = require('./utils/forecast')
 const geocode = require('./utils/geocode')
 
-geocode('Long Beach, CA', (error, data) => {
-  if (error) {
-    console.log(error)
-  } else {
-    console.log('Data', data)
-  }
-})
+// location from command line argument
+const locationString = process.argv.slice(2).join(' ')
+if (locationString === '') {
+  return console.log('Enter a location to search')
+}
 
-forecast(34.0522, -118.2437, (error, data) => {
-  console.log('Error', error)
-  console.log('Data', data)
+geocode(locationString, (error, { latitude, longitude, location }) => {
+  if (error) {
+    return console.log(error)
+  }
+
+  forecast(latitude, longitude, (error, forecastData) => {
+    if (error) {
+      return console.log('Error', error)
+    }
+    
+    console.log(location)
+    console.log(forecastData)
+  })
 })
--- utils/geocode.js
+++ utils/geocode.js
@@ -1,20 +1,23 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-  const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' 
-    + encodeURIComponent(address) 
-    +'.json?access_token=pk.eyJ1IjoibWRlaGVycmVyYSIsImEiOiJjbGJkd3ZlOGMwNW5uM25tZmdrNjdmY3I2In0.9Gsby3FDw3iZpWcGViw4UA&limit=1'
+  const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoibWRlaGVycmVyYSIsImEiOiJjbGJkd3ZlOGMwNW5uM25tZmdrNjdmY3I2In0.9Gsby3FDw3iZpWcGViw4UA&limit=1'
 
-    request({url: url, json: true}, (error, response) => {
+    request({ url, json: true }, (error, { body }) => {
       if (error) {
-        callback('Unable to connect to Mapbox API')
-      } else if (response.body.features.length === 0){
+        return callback('Unable to connect to Mapbox API')
+      }
+
+      const [ longitude, latitude ] = body.features[0].center
+      const location = body.features[0].place_name
+
+      if (body.features.length === 0) {
         callback('No search results found', undefined)
       } else {
         callback(undefined, {
-          latitude: response.body.features[0].center[1],
-          longitude: response.body.features[0].center[0],
-          location: response.body.features[0].place_name
+          latitude,
+          longitude,
+          location
         })
       }
     })
